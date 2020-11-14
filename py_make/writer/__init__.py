f95f4722187dbef4e77304dddb5e3c657da5f510
RTBlanco <ronnytoribio1@hotmail.com>
2020-11-13 22:32:30 -0500

    update

--- py_make/writer/__init__.py
+++ py_make/writer/__init__.py
@@ -1,6 +1,6 @@
 import json
 
-with open('config.json','r') as config_file:
+with open('py_make/config/config.json','r') as config_file:
   config = json.load(config_file)
   author = config["author"]
   author_email= config["author-email"]
