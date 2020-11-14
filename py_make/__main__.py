21abe2352d028138e028e55a9beff14431467a35
RTBlanco <ronnytoribio1@hotmail.com>
2020-11-13 20:44:02 -0500

    fixed script issue

--- py_make/__main__.py
+++ py_make/__main__.py
@@ -2,15 +2,19 @@ import sys
 from make_package.package import package 
 from config.json_writer import update_config
 
-param1 = sys.argv[1]
+def main():
+  param1 = sys.argv[1]
 
-if len(sys.argv) >= 3:
-  project_name_input = sys.argv[2] 
-  version_input = sys.argv[3] if len(sys.argv) == 4 else None
+  if len(sys.argv) >= 3:
+    project_name_input = sys.argv[2] 
+    version_input = sys.argv[3] if len(sys.argv) == 4 else None
 
 
-if param1 == "init":
-  update_config()
-else:
-  package(project_name_input, version_input)
-    
+  if param1 == "init":
+    update_config()
+  else:
+    package(project_name_input, version_input)
+      
+
+if __name__=="__main__":
+  main()
