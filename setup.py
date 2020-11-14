3dc87999ada0efdf0c26d4d1ecd59b3f30d37705
RTBlanco <ronnytoribio1@hotmail.com>
2020-11-13 20:10:25 -0500

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
+import py_make
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="py_make",
-    version="0.0.1",
+    version=py_make.__version__,
     author="RTBlanco",
     author_email="ronnytoribio1@hotmail.com",
     description="TODO:A small example package",
