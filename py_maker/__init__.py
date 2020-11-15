8d5cf6f6287e77b13f586df359e00541d45659da
RTBlanco <ronnytoribio1@hotmail.com>
2020-11-15 08:32:11 -0500

    fixed issue with writing

--- py_maker/__init__.py
+++ py_maker/__init__.py
@@ -1,8 +1,8 @@
 from py_maker.config.json_writer import get_dir
 
-if not get_dir() / "config.json":
+if not (get_dir() / "config.json").exists():
   get_dir().mkdir(parents=True)
   with (get_dir() / "config.json").open("w") as f:
     f.write('{"author": "Username", "author-email": "Username@email.com"}')
 
-__version__ = "0.0.5" 
+__version__ = "0.0.6" 
