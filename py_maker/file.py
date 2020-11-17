74cb194954b24f43aa6e96fd78173fe1b7e16faa
RTBlanco <ronnytoribio1@hotmail.com>
2020-11-17 18:12:15 -0500

    Created the file class

--- py_maker/file.py
+++ py_maker/file.py
@@ -2,6 +2,7 @@ import json
 from datetime import datetime
 from py_maker.make_package.package import Package 
 from py_maker.config.json_writer import get_dir
+from py_maker import Author
 from pkg_resources import resource_string as resource_bytes
 
 # TODO: create an author class 
@@ -15,81 +16,33 @@ author_email = config_data["author-email"]
 # TODO: Write files here 
 # make it so that the arugments are author and projects 
 class Files:
-  def __init__(self, project):
+  def __init__(self, project, author):
     self.project = project
+    self.author = author
+
+  def setup(self,):
+    setup = resource_bytes(__name__, 'templates/setup_template.txt').decode("utf-8")
+    with open(f"{self.project.name}/setup.py", "w") as f:
+      f.write(setup.format(
+        project_name = self.project.name,
+        project = self.project,
+        project_version = self.project.version,
+        author = self.author.name,
+        author_email = self.author.email,
+      ))
+
+  def license(self):
+    license = resource_bytes(__name__, 'templates/license_template.txt').decode("utf-8")
+    with open(f"{self.project.name}/LICENSE", "w") as f:
+      f.write(license.format(date = datetime.now().year, author = self.author.name))
 
 
-  def setup(self, path):
-    setup = resource_bytes('py_make.__main__', 'templates/setup_template.txt').decode("utf-8")
-    with open(f"{self.project}.name/setup.py", "w") as f:
-      f.write()
+  def readme(self):
+    readme = resource_bytes(__name__, 'templates/readme_template.txt').decode("utf-8")
+    with open(f"{self.project.name}/README.md", "w") as f:
+      f.write(readme.format(project_name = self.project.name))
 
 
-  #   f"""import setuptools
-  # import {self.project.name}
-    
-  # with open("README.md", "r") as fh:
-  #   long_description = fh.read()
-
-  # setuptools.setup(
-  #   name="{self.project.name}",
-  #   version={self.project}.__version__,
-  #   author="{author}",
-  #   author_email="{author_email}",
-  #   description="TODO:A small example package",
-  #   long_description=long_description,
-  #   long_description_content_type="text/markdown",
-  #   url="TODO: Enter URL",
-  #   packages=setuptools.find_packages(),
-  #   classifiers=[
-  #       "Programming Language :: Python :: 3",
-  #       "License :: OSI Approved :: MIT License",
-  #       "Operating System :: OS Independent",
-  #   ],
-  #   python_requires='>=3.6',    
-  # )"""
-
-
-
-  def license_writer():
-    return f"""Copyright (c) {datetime.now().year} {author}
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE."""
-
-
-  def readme_writer(self):
-    return f"""# {self.project.capitalize()}
-
-  :TODO  write a description
-
-
-  ## Installation
-
-      $ pip install {self.project.name}
-
-  ## Usage
-  TODO: write quick tutorial 
-
-
-  This is a simple example package. You can use
-  [Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
-  to write your content."""
-
-  def init_writer(self):
-      return f"__version__ = '{self.project.version}'"
+  def init(self):
+    with open(f"{self.project.name}/{self.project.name}/__init__.py") as f:
+      f.write(self.project.version)
