fe026c5e51159b03001fb9fd74c6412ef72df8e7
RTBlanco <ronnytoribio1@hotmail.com>
2021-04-29 01:16:43 -0400

    printed confirmation that file was created

--- py_maker/file.py
+++ py_maker/file.py
@@ -15,19 +15,21 @@ class Files:
         author = self.author.name,
         author_email = self.author.email,
       ))
+    print(f"Created -> {self.project.name}/setup.py ")
 
   def license(self):
     license = resource_bytes(__name__, 'templates/license_template.txt').decode("utf-8")
     with open(f"{self.project.name}/LICENSE", "w") as f:
       f.write(license.format(date = datetime.now().year, author = self.author.name))
-
+    print(f"Created -> {self.project.name}/LICENSE")
 
   def readme(self):
     readme = resource_bytes(__name__, 'templates/readme_template.txt').decode("utf-8")
     with open(f"{self.project.name}/README.md", "w") as f:
       f.write(readme.format(project_name = self.project.name))
-
+    print(f"Created -> {self.project.name}/README.md")
 
   def init(self):
     with open(f"{self.project.name}/{self.project.name}/__init__.py", "w") as f:
       f.write(f'__version__ = "{self.project.version}"')
+    print(f"Created -> {self.project.name}/{self.project.name}/__init__.py")
